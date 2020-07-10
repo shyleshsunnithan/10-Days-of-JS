37408f04f531b92e1a17e0a1745817ee9769fb29
Shylesh S <37534898+shyleshsunnithan@users.noreply.github.com>
2020-07-10 14:23:19 +0530

    Day 6 Exnclose in Brackets

---
+++ Day6EncloseInBrackets/Method2/main.js
@@ -0,0 +1,26 @@
+function encloseInBrackets(str) {
+    const value = '(' + str + ')';
+
+    return value;
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('encloseInBrackets()', () => {
+    it('adds () around a string', () => {
+        // arrange
+        const str = "Yo";
+        
+        // act
+        const result = encloseInBrackets(str);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toBe('(Yo)');
+    });
+});
